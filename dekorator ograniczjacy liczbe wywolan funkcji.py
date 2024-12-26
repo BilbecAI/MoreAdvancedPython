b207699640b795c89808f448829fc0af9a12bce9
Dawid Majcher <dawid-28082003@wp.pl>
2024-12-26 14:40:39 +0100

    Update dekorator ograniczjacy liczbe wywolan funkcji.py

--- dekorator ograniczjacy liczbe wywolan funkcji.py
+++ dekorator ograniczjacy liczbe wywolan funkcji.py
@@ -1,13 +1,15 @@
-def decorator(func):
-    count=0
-    def wrapper(*args, **kwargs):
-        nonlocal count
-        count+=1
-        limit=3
-        result=func(*args,**kwargs)
-        if count>limit:
+#Stwórz dekorator, który pozwala funkcji być wywołaną maksymalnie określoną liczbę razy. Po przekroczeniu limitu wyświetla komunikat i nie wykonuje funkcji.
+
+def decorator(func): #tworzymy dekorator który jako argument przyjmuje funkcje
+    count=0 #zaczynamy liczyć
+    limit=3 # ustawiamy limit
+    def wrapper(*args, **kwargs): # tworzymy dekorator ktory jako argument przyjmuje argumenty funkcji
+        nonlocal count      
+        if count>limit: # warunek jeżeli liczenie przekroczy limit wypluwamy komunikat 
             print ("Limit exceeded")
         else:
+            count+=1 # zwiekszamy o 1 
+            result=func(*args,**kwargs) # wynik funkcji
             return result
     return wrapper 
 @decorator
