bc8909eee4e65e726fc16a0dd64cb924cc75bb7f
Dawid Majcher <dawid-28082003@wp.pl>
2024-12-26 14:14:58 +0100

    Update dzielenie_przez_0.py

--- dzielenie_przez_0.py
+++ dzielenie_przez_0.py
@@ -1,6 +1,9 @@
+#Swtórz dekortor, który przechwytuje wyjątki wypluwane przez funkcje i wyświetla odpowiedni komunikat. 
+
+
 def decorator(func): #Tworzymy dekorator który za argument przyjmuje funkcje
     def wrapper(*args, **kwargs): #Musimy stworzyc funkcje wewnetrzną która za argumenty przyjmuje dowolne argumenty z funkcji divide
-        if any(arg == 0 for arg in args): #Sprwdzamy czy któryś z argumentów jest równy zero
+        if any(arg == 0 for arg in args): #Sprawdzamy czy któryś z argumentów jest równy zero
             print("Nie można dzielić przez 0")
         else:
             return func(*args, **kwargs)
